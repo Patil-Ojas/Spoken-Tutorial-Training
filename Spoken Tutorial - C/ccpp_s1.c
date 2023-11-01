48d4c1035fdead5b848446b18a56e5c84899f4f7
Ojas Patil <128805590+Patil-Ojas@users.noreply.github.com>
2023-11-01 09:46:18 +0530

    Add files via upload

---
+++ Spoken Tutorial - C/ccpp_s1.c
@@ -0,0 +1,11 @@
+// me farst c program
+
+#include <stdio.h>
+
+int main()
+{
+    printf("sup world \n");
+    printf("This is gonna be printed");
+    return 0;
+    printf("This not gonna be printed");
+}
---
+++ Spoken Tutorial - C/ccpp_s2.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+//#include <conio.h>
+using namespace std;
+
+int main()
+{
+    cout<<"sup world";
+    return 0;
+}
+
+// aha
+/* multi
+line 
+comment
+*/
